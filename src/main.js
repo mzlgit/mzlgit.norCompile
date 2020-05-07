f99c757d9315f43822d39a8a2254eee2fec5a5ac
magj <magj@minstone.com.cn>
2020-05-07 10:23:30 +0800

    采用vuetifyjs

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import store from './store'
 import router from './router'
 // 样式
 import "./less/index.less";
+
 //rem
 import computed_rem from './utils/rem'
+
+import vuetify from './plugins/vuetify';
 //计算rem
 computed_rem()
 
@@ -15,5 +18,6 @@ Vue.config.productionTip = false
 new Vue({
   store,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-    devServer: {
-      port: 8086,
-      open: true
-    },
-    // configureWebpack: {
-    //   resolve: {
-    //     alias: {
-    //       'vue$': 'vue/dist/vue.esm.js' 
-    //     }
-    //   }
-    // }
-  }
+  "devServer": {
+    "port": 8086,
+    "open": true
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
